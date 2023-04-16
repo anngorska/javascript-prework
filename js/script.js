601eaceb15104d4ef6f557ba50f3795950a1968b
Anna Górska <annagorska.arch@gmail.com>
2023-04-16 19:07:32 +0200

    Change js code

--- js/script.js
+++ js/script.js
@@ -8,7 +8,16 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var playerMove;
-playerMove = 'papier';
-computerMove = 'kamień';
-printMessage('Zagrałem ' + playerMove + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
